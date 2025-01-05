3f5fe0e07024e26130543b5cc6d7132256df60af
iloveu3000x <108974317+iloveu3000x@users.noreply.github.com>
2025-01-06 01:04:38 +0900

    20250106-Update index.js
    WEEK 1

--- week1/index.js
+++ week1/index.js
@@ -8,6 +8,14 @@
 
 // 1.1 範例
 const alexAge = 25;
+const alexMembershipID = "GYM2024-12345";
+const isRunningOnTreadmill = true;
+
+console.log(alexAge,alexMembershipID,isRunningOnTreadmill);
+// ↓
+// 25 'GYM2024-12345' true
+
+
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,10 +24,12 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaFee = 300;
+const fitnessTrainingFee = 500;
+const fitnessSingleTutoring = 1500;
+const fitnessBudget = 3000;
+
+
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
@@ -27,17 +37,48 @@ const 變數四 = 3000;
 // 條件二：瑜伽團課只能一次買 3 堂
 // 條件三：一定要花到 2400 以上，並購買 5 堂課程
 
+const yogaFee = 300*3;
+const fitnessTrainingFee = 500;
+const fitnessSingleTutoring = 1500;
+const fitnessBudget = 3000;
+let courseFeesTotal = yogaFee + fitnessTrainingFee + fitnessSingleTutoring;
+let AlexBudget = fitnessBudget - courseFeesTotal ;
+
 console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
+// ↓
+// Alex 買完課程了，他一共剩下 100 元
+
+
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
+
+const discount = 0.9;
+let coursePrice = 200;
+let courseNum = 14;
+let setPrice = discount*coursePrice*courseNum;
+
+console.log(setPrice);
+// ↓
+// 2520
+
+
 
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
 // 4-2. 目前一起等待的機車有 8 台
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+
+let isWaiting = true;
+let redLight = true;
+let remainingTime = 28;
+let motorWaitingNum = 8;
+let cloudNum = 5;
+const sunNum = 1;
+
+
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,8 +91,15 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+myWater -= 800; // 中午喝了 800cc
+myWater += 1000; // 下午裝了 1000cc
+myWater -= 700; // 健身喝了 700cc
 
 console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
+// ↓
+// Alex 的水壺還有 1000cc 的水
+
+
 
 // ### 題目七：情境題：變數計算
 // 情境：Anna 每週都會到單次計費型的健身房運動，週日運動結束後，想知道自己本週的消費金額，但結帳系統出了點問題，Anna 決定自己用 JS 來計算。
@@ -62,20 +110,32 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
+const groupClassesFee = 150;
+let machineUsePriceTotal = machineUsePrice*3;
+const groupClassesTotal = groupClassesFee*2;
+let total = machineUsePriceTotal+groupClassesTotal;
 
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
 );
+// ↓
+// Anna 本週器械使用費共 150 元，團體課費用共 300 元，一共消費金額是 450元
+
+
 
 // ### 題目八：變數重新賦予值
 // 情境：請依照以下程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 let a = 8; // 範例：宣告了一個 a 的變數，並賦予了一個 8 的數字型別
-let b = 0;
-a = 13;
-a = b + 4;
-a - b;
-b += 1;
+let b = 0; // 宣告了一個 b 的變數，並賦予了一個 0 的數字型別
+a = 13; // 重新賦予了 a 變數，一個 13 的數字型別
+a = b + 4; // 重新賦予了 a 變數，等於 b 變數加上 4 的值
+a - b; // 僅計算了 a 變數減 b 的變數，任一變數皆無被賦予任何值，故不影響結果
+b += 1; // 計算 b 變數加上 1 的值，並重新賦予 b 變數
+
+console.log(a,b); // 輸出 a 變數與 b 變數
+
+
 
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
@@ -87,15 +147,24 @@ let g = d + d;
 let h = f + g;
 
 // 請從以下新增註解，告知上面每行各別是哪些型別
-// a 是 string
-// b 是 ???
+// a 是 數字number
+// b 是 數字number
+// c 是 字串string
+// d 是 數字number
+// e 是 字串string
+// f 是 布林boolean
+// g 是 數字number
+// h 是 數字number
+
+
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 
-let numberArr1 = [5, 10, 15];
-let numberArr2 = numberArr1;
-numberArr2.push(20);
-numberArr2 = [25, 30, 35];
-console.log(numberArr1, numberArr2);
+let numberArr1 = [5, 10, 15]; // 宣告陣列 numberArr1 ，初始值設定為 '5'  '10'  '15'
+let numberArr2 = numberArr1; // 宣告變數 numberArr2 ，將 numberArr1 記憶體位址賦予numberArr2
+numberArr2.push(20); // 變數 numberArr2 新增一個 20 的值，並也傳至 numberArr1 記憶體位址，新增了這個值
+numberArr2 = [25, 30, 35]; // 將變數 numberArr2 重新賦值為一個新的陣列 '25'  '30'  '35'
+console.log(numberArr1, numberArr2); // 輸出 numberArr1 與 numberArr2
+// [5, 10, 15, 20]  [25, 30, 35]
